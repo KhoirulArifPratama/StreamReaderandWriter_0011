3e666afbe91c2cb68b8675a42994700be169f92e
KhoirulArifPratama <khoirularif932@gmail.com>
2025-06-09 10:58:31 +0700

    Menambahkan instruksi keluar saat input file

--- ioFile.cpp
+++ ioFile.cpp
@@ -2,3 +2,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int main()
+{
+    string baris;
+
+    ofstream outfile;
+
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
