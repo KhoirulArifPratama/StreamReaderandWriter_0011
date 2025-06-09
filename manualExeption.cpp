22537f18e31b6fe62e84a258ed1d5f5b322abea6
KhoirulArifPratama <khoirularif932@gmail.com>
2025-06-09 11:08:49 +0700

    Menambahkan contoh penanganan exception dengan multiple catch

--- manualExeption.cpp
+++ manualExeption.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+
+    try
+    {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5;
+        cout << "pernyataan tidak akan diexekusi" << endl;
+    }
+    catch (int a)
+    {
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch (...)
+    {
+        cout << "default pengecualian diexekusi" << endl;
+    }
+
+    return 0;
+}
