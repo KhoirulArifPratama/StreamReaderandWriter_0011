fa1fa65e3d69c4d7e74bccc80160a7f7bbca450f
KhoirulArifPratama <khoirularif932@gmail.com>
2025-06-09 10:59:52 +0700

    Input ke file dan baca ulang isinya

--- ioFile.cpp
+++ ioFile.cpp
@@ -12,3 +12,39 @@ int main()
     outfile.open("contohfile.txt");
 
     cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+    while (true)
+    {
+        cout << "- ";
+
+        getline(cin, baris);
+
+        if (baris == "q")
+            break;
+
+        outfile << baris << endl;
+    }
+
+    outfile.close();
+
+    ifstream infile;
+
+    infile.open("contohfile.txt");
+
+    cout << endl
+         << ">= Membuka dan membaca file " << endl;
+
+    if (infile.is_open())
+    {
+        while (getline(infile, baris))
+        {
+            cout << baris << '\n';
+        }
+
+        infile.close();
+    }
+
+    else
+        cout << "Unable to open file";
+    return 0;
+}
